6e4f4d86bca12e546b88f1f079a6272d2c2944c4
Jakub Ruchel <jruchel@gmail.com>
2021-04-29 16:01:52 +0200

    Changed font to nunito-black

--- src/App.vue
+++ src/App.vue
@@ -1,7 +1,7 @@
 <template>
   <v-app data-app style="background-color: #9FA8DA">
     <v-main>
-      <h1 style="text-align: center">Tic Tac Toe</h1>
+      <h1 style="text-align: center; margin-top: 0.6%">Tic Tac Toe</h1>
       <TicTacToe :size="3"></TicTacToe>
     </v-main>
   </v-app>
@@ -17,4 +17,20 @@ export default {
 
 //TODO highlight tiles after game over
 </script>
+<style lang="scss">
+@mixin setFont($font-name) {
+  font-family: $font-name;
+  src: local($font-name),
+  url(./assets/fonts/#{$font-name}.ttf) format("truetype");
+}
+
+@font-face {
+  @include setFont("Nunito-Black")
+}
+
+*{
+  font-family: Nunito-Black;
+}
+
+</style>
 
